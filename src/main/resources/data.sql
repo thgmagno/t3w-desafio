3c130de944d49c75977add282d9d08b50aa63fbb
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2025-01-23 21:52:47 -0300

    script de criação e carga de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,22 @@
 INSERT INTO pessoa (cpf, nome)
 VALUES
     ('56637927082', 'Pessoa 1'),
-    ('56637927082', 'Pessoa 2');
+    ('56637927082', 'Pessoa 2'),
+    ('12312312312', 'Pessoa 3');
 
 INSERT INTO produto (descricao, valor_unitario)
 VALUES
     ('Produto 1', 10.0),
     ('Produto 2', 20.0),
-    ('Produto 3', 20.0)
-;
+    ('Produto 3', 20.0);
+
+INSERT INTO pedido (pessoa_id, valor_total)
+VALUES
+    (1, 30.0),
+    (2, 20.0);
+
+INSERT INTO pedido_item (pedido_id, produto_id, quantidade)
+VALUES
+    (1, 1, 1),
+    (1, 2, 1),
+    (2, 3, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,4 +12,22 @@ CREATE TABLE produto
     valor_unitario NUMERIC(10, 2) NOT NULL
 );
 
+CREATE TABLE pedido
+(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pessoa_id INTEGER NOT NULL,
+    valor_total NUMERIC(10, 2) NOT NULL
+);
 
+ALTER TABLE pedido ADD CONSTRAINT fk_pessoa FOREIGN KEY (pessoa_id) REFERENCES pessoa(id) ON DELETE CASCADE;
+
+CREATE TABLE pedido_item
+(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    pedido_id INTEGER NOT NULL,
+    produto_id INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL
+);
+
+ALTER TABLE pedido_item ADD CONSTRAINT fk_pedido FOREIGN KEY (pedido_id) REFERENCES pedido(id) ON DELETE CASCADE;
+ALTER TABLE pedido_item ADD CONSTRAINT fk_produto FOREIGN KEY (produto_id) REFERENCES produto(id) ON DELETE CASCADE;
